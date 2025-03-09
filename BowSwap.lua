d4570b46c90dca4acc37d144ca56685e10daebbd
Sica <sica@cook.as>
2025-03-09 01:17:46 +0100

    Increase popup text duration

--- BowSwap.lua
+++ BowSwap.lua
@@ -1,87 +1,87 @@
 local BowSwap = {}
 
-function BowSwap.Equip(args)
+function BowSwap.Equip( args )
   if BowSwap.bag and BowSwap.slot then
     BowSwap.stored = false
 
-    PickupContainerItem(BowSwap.bag, BowSwap.slot)
-    PickupInventoryItem(18)
+    PickupContainerItem( BowSwap.bag, BowSwap.slot )
+    PickupInventoryItem( 18 )
 
     if args ~= 'nowarn' then
-      BowSwap.Alert( BowSwap.bowType ..' equipped!', 'green')
+      BowSwap.Alert( BowSwap.bowType .. ' equipped!', 'green' )
     end
   end
 end
 
-function BowSwap.Unequip(args)
-  local itemLink = GetInventoryItemLink("player", 18)
+function BowSwap.Unequip( args )
+  local itemLink = GetInventoryItemLink( "player", 18 )
   if not itemLink then return end
-  
+
   local bag, slot = BowSwap.FindEmptyNormalBagSlot()
   if bag and slot then
     BowSwap.stored = true
     BowSwap.bag = bag
     BowSwap.slot = slot
-  
-    PickupInventoryItem(18)
-    PickupContainerItem(BowSwap.bag, BowSwap.slot)
+
+    PickupInventoryItem( 18 )
+    PickupContainerItem( BowSwap.bag, BowSwap.slot )
 
     if args ~= 'nowarn' then
       local itemId = BowSwap.GetItemIdFromLink( itemLink )
       local _, _, _, _, _, sType = GetItemInfo( itemId )
 
-      BowSwap.bowType = string.sub(sType, 1, -2)
-      BowSwap.Alert( BowSwap.bowType..' unequipped!')
+      BowSwap.bowType = string.sub( sType, 1, -2 )
+      BowSwap.Alert( BowSwap.bowType .. ' unequipped!' )
     end
   else
-    DEFAULT_CHAT_FRAME:AddMessage("|cffff0000WARN: No empty inventory slot!|r")
+    DEFAULT_CHAT_FRAME:AddMessage( "|cffff0000WARN: No empty inventory slot!|r" )
   end
 end
 
-function BowSwap.Alert(msg, color)
+function BowSwap.Alert( msg, color )
   if not BowSwap.messageFrame then
-    BowSwap.messageFrame = CreateFrame("Frame", nil, UIParent)
-    BowSwap.messageText = BowSwap.messageFrame:CreateFontString(nil, "OVERLAY", "GameFontNormalHuge")
+    BowSwap.messageFrame = CreateFrame( "Frame", nil, UIParent )
+    BowSwap.messageText = BowSwap.messageFrame:CreateFontString( nil, "OVERLAY", "GameFontNormalHuge" )
   end
 
-  local messageFrame = BowSwap.messageFrame  
-  messageFrame:SetWidth(400)
-  messageFrame:SetHeight(100)
-  messageFrame:SetPoint("CENTER", 0, 100)
+  local messageFrame = BowSwap.messageFrame
+  messageFrame:SetWidth( 400 )
+  messageFrame:SetHeight( 100 )
+  messageFrame:SetPoint( "CENTER", 0, 100 )
 
   local messageText = BowSwap.messageText
-  messageText:SetPoint("CENTER", 0, 0)
+  messageText:SetPoint( "CENTER", 0, 0 )
   if color == 'green' then
-    messageText:SetText("|cff00ff00" .. msg .. "|r")
+    messageText:SetText( "|cff00ff00" .. msg .. "|r" )
   else
-    messageText:SetText("|cffff0000" .. msg .. "|r")
+    messageText:SetText( "|cffff0000" .. msg .. "|r" )
   end
 
   local elapsed = 0
-  local visibleDuration = 0.8
+  local visibleDuration = 2
   local fadeDuration = 0.6
 
-  messageFrame:SetScript("OnUpdate", function(self, delta)
+  messageFrame:SetScript( "OnUpdate", function( self, delta )
     elapsed = elapsed + arg1
     if elapsed < (visibleDuration + fadeDuration) then
       local alpha = 1 - ((elapsed - visibleDuration) / fadeDuration)
-      messageText:SetTextColor(0, 0, 0, alpha)
+      messageText:SetTextColor( 0, 0, 0, alpha )
     else
-      messageText:SetText("")
-      messageFrame:SetScript("OnUpdate", nil)
+      messageText:SetText( "" )
+      messageFrame:SetScript( "OnUpdate", nil )
     end
-  end)
+  end )
 end
 
 function BowSwap.OnLoad()
-  DEFAULT_CHAT_FRAME:AddMessage("BowSwap Loaded")
+  DEFAULT_CHAT_FRAME:AddMessage( "BowSwap Loaded" )
 
   SLASH_bowswap1 = "/bowswap"
-  SlashCmdList["bowswap"] = function(args)
+  SlashCmdList[ "bowswap" ] = function( args )
     if BowSwap.stored then
-      BowSwap.Equip(args)
+      BowSwap.Equip( args )
     else
-      BowSwap.Unequip(args)
+      BowSwap.Unequip( args )
     end
   end
 end
@@ -94,14 +94,14 @@ function BowSwap.eventHandler()
   end
 end
 
-function BowSwap.GetItemIdFromLink(itemLink)
-  return itemLink and string.match(itemLink, "item:(%d+)")
+function BowSwap.GetItemIdFromLink( itemLink )
+  return itemLink and string.match( itemLink, "item:(%d+)" )
 end
 
 function BowSwap.FindEmptyNormalBagSlot()
   for bag = 0, 4 do
-    local bagName = GetBagName(bag)
-    if bagName and not string.find(bagName, "Quiver") and not string.find(bagName, "Pouch") and not string.find(bagName, "Soul") then      
+    local bagName = GetBagName( bag )
+    if bagName and not string.find( bagName, "Quiver" ) and not string.find( bagName, "Pouch" ) and not string.find( bagName, "Soul" ) then
       for slot = 1, GetContainerNumSlots( bag ) do
         if not GetContainerItemLink( bag, slot ) then
           return bag, slot
@@ -112,6 +112,6 @@ function BowSwap.FindEmptyNormalBagSlot()
   return nil, nil
 end
 
-BowSwap.Frame = CreateFrame("FRAME")
-BowSwap.Frame:RegisterEvent("ADDON_LOADED")
-BowSwap.Frame:SetScript("OnEvent", BowSwap.eventHandler)
+BowSwap.Frame = CreateFrame( "FRAME" )
+BowSwap.Frame:RegisterEvent( "ADDON_LOADED" )
+BowSwap.Frame:SetScript( "OnEvent", BowSwap.eventHandler )
